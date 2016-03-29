bdddea71be16c5e696bcdf98e7fbc8e6fa240715
Andrew Wetmore <Alwaygetgo@gmail.com>
2016-03-29 12:50:44 -0400

    Added animations for player movement, Made palyer movement run in animation for smoothness. Added stack of controls for backtracking.

--- static/js/dungeon.js
+++ static/js/dungeon.js
@@ -1,31 +1,10 @@
-var dungeonStage, dungeonLayer, characterLayer, player;
-
-// Move the player a certain distance with checking for invaild spaces
-function move(x, y) {
-	player.x(player.x()+x);
-	player.y(player.y()+y);
-	var found = false;
-	for (i=0;i<dungeon.walls.length && !found;i++){
-		if((dungeon.walls[i].x==Math.trunc((player.x())/scale) || dungeon.walls[i].x==Math.trunc((player.x()+player.getWidth())/scale)) && 
-		(dungeon.walls[i].y==Math.trunc((player.y())/scale) || dungeon.walls[i].y==Math.trunc((player.y()+player.getHeight())/scale))){
-			found = true;
-			player.x(player.x()-x);
-			player.y(player.y()-y);
-		}
-	}
-	if(!found){
-		characterLayer.draw();
-		var viewport = document.getElementById("viewport");
-		viewport.scrollLeft = player.getAbsolutePosition().x-viewport.clientWidth/2;
-		viewport.scrollTop = player.getAbsolutePosition().y-viewport.clientHeight/2;
-	}
-}
+var dungeonStage, dungeonLayer, characterLayer, player, playerMovements = [], curMovement, dirKeyStack = [];
+const DIRECTIONS = ['Down', 'Left', 'Right', 'Up'], CONTROLS = {'38': 3, '87': 3, '40': 0, '83': 0, '37': 1, '65': 1, '39': 2, '68': 2};
 
 // Gets a sprite's current width
 Konva.Sprite.prototype.getWidth = function(){
 	return this.animations()[this.animation()][this.frameIndex()*4+2]*this.scaleX();
 }
-
 
 // Gets a sprite's current height
 Konva.Sprite.prototype.getHeight = function(){
@@ -34,6 +13,13 @@ Konva.Sprite.prototype.getHeight = function(){
 
 // Create the basic grid with the player after the page has loaded
 document.addEventListener('DOMContentLoaded', function() {
+	
+	// Set all keys to false
+	keys = [];
+	for(var i=0;i<=222;i++)
+		keys[i] = false;
+	
+	// Create the stage and layers
 	dungeonStage = new Konva.Stage({
 	  container: 'grid',
 	  width: dungeon.width*scale,
@@ -42,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	dungeonLayer = new Konva.Layer();
 	characterLayer = new Konva.Layer();
 	
+	// Create all the paths first since rooms should be over them
 	for (var i in dungeon.paths)
 		dungeonLayer.add(new Konva.Rect({
 						      x: dungeon.paths[i].x*scale,
@@ -52,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
 						      strokeWidth: 0
 						    }));
 	
+	// Create all the rooms next
 	for (var i in dungeon.rooms)
 		dungeonLayer.add(new Konva.Rect({
 						      x: dungeon.rooms[i].X*scale,
@@ -62,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
 						      strokeWidth: 0
 						    }));
 	
+	// Create all the walls last
 	for (var i in dungeon.walls)
 		dungeonLayer.add(new Konva.Rect({
 						      x: dungeon.walls[i].x*scale,
@@ -71,10 +60,16 @@ document.addEventListener('DOMContentLoaded', function() {
 						      fill: 'yellow',
 						      strokeWidth: 0
 						    }));
+						      
+	
+	// Add the layer with all the background to the stage (rooms, paths, walls, etc.)
+	dungeonStage.add(dungeonLayer);
 						    
+	// Create the sprite for the player
 	var playerImg = new Image();
     playerImg.onload = function() {
 
+	  // Actually create the player's sprite with animations
       player = new Konva.Sprite({
         x: xPos,
         y: yPos,
@@ -118,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function() {
 				        32, 96, 32, 32
 				      ]
 				    },
-        frameRate: 7,
+        frameRate: 10,
         frameIndex: 0,
 		          scale: { x:scale/64, y:scale/64 }
       });
@@ -128,36 +123,106 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // add the layer to the stage
       dungeonStage.add(characterLayer);
-
-      // start sprite animation
-      player.start();
       
-	  var viewport = document.getElementById("viewport");
-	  viewport.scrollLeft = player.getAbsolutePosition().x-viewport.clientWidth/2;
-	  viewport.scrollTop = player.getAbsolutePosition().y-viewport.clientHeight/2;
+	  // Update the viewport
+	  updateViewport();
     };
-    
     playerImg.src = 'static/images/$placeholder_sprite.png';
+    
+    // Create the player movement animations
+    playerMovements.push(new Konva.Animation(function(frame) {
+	        move(player, 0, frame.timeDiff*playerSpeed);
+	        updateViewport();
+	    }, characterLayer));
+	playerMovements.push(new Konva.Animation(function(frame) {
+	        move(player, 1, frame.timeDiff*playerSpeed);
+	        updateViewport();
+	    }, characterLayer));
+	playerMovements.push(new Konva.Animation(function(frame) {
+	        move(player, 2, frame.timeDiff*playerSpeed);
+	        updateViewport();
+	    }, characterLayer));
+	playerMovements.push(new Konva.Animation(function(frame) {
+	        move(player, 3, frame.timeDiff*playerSpeed);
+	        updateViewport();
+	    }, characterLayer));
 	
-	dungeonStage.add(dungeonLayer);
 }, false);
+
+// Move the given sprite a certain direction with checking for invaild spaces
+function move(sprite, dir, distance) {
+	var x = dir==1 ? -distance : (dir==2 ? distance : 0);
+	var y = dir==0 ? distance : (dir==3 ? -distance : 0);
+	sprite.x(sprite.x()+x);
+	sprite.y(sprite.y()+y);
+	for (var i=0, found=false;i<dungeon.walls.length && !found;i++){
+		if((dungeon.walls[i].x==Math.trunc((sprite.x())/scale) || dungeon.walls[i].x==Math.trunc((sprite.x()+sprite.getWidth())/scale)) && 
+		(dungeon.walls[i].y==Math.trunc((sprite.y()+sprite.getHeight()/2)/scale) || dungeon.walls[i].y==Math.trunc((sprite.y()+sprite.getHeight())/scale))){
+			found = true;
+			sprite.x(sprite.x()-x);
+			sprite.y(sprite.y()-y);
+		}
+	}
+}
+
+// Udate the player's viewport onto the player
+function updateViewport(){
+	var viewport = document.getElementById("viewport");
+	viewport.scrollLeft = player.getAbsolutePosition().x-viewport.clientWidth/2;
+	viewport.scrollTop = player.getAbsolutePosition().y-viewport.clientHeight/2;
+}
+
+// Move the player a certain direction with checking for invaild spaces and playing an animation (0 - down, 1 - left, 2 - right, 3 - down)
+function movePlayer(dir){
+	
+	// if not animation set to moving direction set it and start the animation
+	if(player.animation()!='move'+DIRECTIONS[dir]){
+		var start = player.animation().startsWith('idle');
+		player.setAnimation('move'+DIRECTIONS[dir]);
+		if(start)
+			player.start();
+		if(curMovement)
+			curMovement.stop();
+		curMovement = playerMovements[dir];
+		playerMovements[dir].start();
+	}
+}
+
+// Stop the player moving in the given direction
+function stopPlayer(){
+	if(curMovement)
+		curMovement.stop();
+	player.setAnimation('idle'+player.animation().substring('move'.length, player.animation().length-1));
+	player.stop();
+}
 
 // Add Key input
 document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 38 || event.keyCode == 87) {
+	if(CONTROLS[event.keyCode.toString()]!=null){
     	event.preventDefault();
-        move(0, -1);
-    }
-    else if(event.keyCode == 40 || event.keyCode == 83) {
-    	event.preventDefault();
-        move(0, 1);
-    }
-    else if(event.keyCode == 37 || event.keyCode == 65) {
-    	event.preventDefault();
-        move(-1, 0);
-    }
-    else if(event.keyCode == 39 || event.keyCode == 68) {
-    	event.preventDefault();
-        move(1, 0);
-    }
+		addKeyToStack(event.keyCode);
+	}
 });
+document.addEventListener('keyup', function(event) {
+	if(CONTROLS[event.keyCode.toString()]!=null){
+    	event.preventDefault();
+		removeKeyFromStack(event.keyCode);
+	}
+});
+
+function addKeyToStack(key){
+	if(dirKeyStack.indexOf(key)==-1)
+		dirKeyStack.push(key);
+	if(CONTROLS[key.toString()]!=CONTROLS[dirKeyStack[dirKeyStack.length-2]])
+		movePlayer(CONTROLS[key.toString()]);
+}
+
+function removeKeyFromStack(key){
+	if(dirKeyStack.indexOf(key)==-1)
+		return;
+	if(dirKeyStack.indexOf(key)==dirKeyStack.length-1 && dirKeyStack.length>1)
+		movePlayer(CONTROLS[dirKeyStack[dirKeyStack.length-2]]);
+	dirKeyStack.splice(dirKeyStack.indexOf(key), 1);
+	if(dirKeyStack.length==0)
+		stopPlayer();
+}
